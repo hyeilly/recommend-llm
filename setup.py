b43e691ead9470aa683ea91b52cce7441f2b5e7f
hyeilly <dev.jiine@gmail.com>
2025-01-22 16:33:36 +0900

    setup.py 내용 수정

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="recommend_llm",
+    name="recommend-llm",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         "fastapi",
         "uvicorn",
-    ],
-    entry_points={
-        'console_scripts': [
-            'service-repo-1 = main:app',
-        ],
-    },
+    ]
 )
