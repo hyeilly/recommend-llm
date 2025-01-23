a88b4a36d761b4cf5e01c51e1deb452ede4d7998
hyeilly <dev.jiine@gmail.com>
2025-01-23 10:51:31 +0900

    edit

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ async def example():
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8001)
