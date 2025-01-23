7fb61cf8809bb011af45c89b145b5bc1a65d3947
hyeilly <dev.jiine@gmail.com>
2025-01-23 14:10:22 +0900

    폴더 구조 변경

--- recommend_llm/main.py
+++ recommend_llm/main.py
@@ -13,9 +13,6 @@ async def example():
     return {"message": "This is the example endpoint zxc0585"}
 
 
-
-
-
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run(app, host="0.0.0.0", port=8001)
