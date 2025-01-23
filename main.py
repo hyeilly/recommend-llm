746424f83ca578356c3ffeb0d9a228e3aa5add1f
hyeilly <dev.jiine@gmail.com>
2025-01-23 10:51:20 +0900

    수정

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, APIRouter
 
+app = FastAPI(title="recommend-llm", description="LLM 추천 프로젝트 API 서비스")
 router = APIRouter()
 
+app.include_router(router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to recommend-llm API"}
@@ -11,10 +13,9 @@ async def example():
     return {"message": "This is the example endpoint zxc0585"}
 
 
-app = FastAPI(title="recommend-llm", description="LLM 추천 프로젝트 API 서비스")
-app.include_router(router)
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8001)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
