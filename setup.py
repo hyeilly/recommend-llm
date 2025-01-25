858f15ff3919adce92f3611ee2bd7e0413722b9a
JIHYE KIM <dev.jiine@gmail.com>
2025-01-25 14:58:08 +0900

    django 설정 추가

--- setup.py
+++ setup.py
@@ -5,7 +5,19 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
+        "djangorestframework"
         "fastapi",
         "uvicorn",
-    ]
+        "pydandtic"
+    ],
+    description="recommend_llm service for Django and FastAPI",
+    author="hyeilly",
+    author_email="dev.jiine@gmail.com",
+    url="https://github.com/hyeilly/recommend-llm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
