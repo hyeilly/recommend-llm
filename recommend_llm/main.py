33f4aaf267ccfff6c88282f8492a6ecadcdc49d7
hyeilly <dev.jiine@gmail.com>
2025-02-03 10:59:21 +0900

    edit

--- recommend_llm/main.py
+++ recommend_llm/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from core_api.app.services.test import TestPrinter
 
 app = FastAPI(title="recommend-llm", description="LLM 추천 프로젝트 API 서비스")
 router = APIRouter()
@@ -11,6 +12,16 @@ async def root():
 @router.get("/zxc0585")
 async def example():
     return {"message": "This is the example endpoint zxc0585"}
+
+# @app.get("/")
+# async def root():
+#     printer = TestPrinter()
+#     printer.print_test()
+#     return {"message": "Welcome to recommend-llm API123123"}
+#
+# @app.get("/zxc0585")
+# async def example():
+#     return {"message": "This is the example endpoint zxc0585"}
 
 
 # if __name__ == "__main__":
